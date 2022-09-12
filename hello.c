8d34f5c214f86358831d6a1221321fd1c10a7368
Carlos Tafur <ctafur@gmail.com>
2022-09-12 18:28:14 +0200

    Añadido un minor.

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@
 
 int main(int argc, char **argv) {
   printf("Hello, I'm Carlos Tafur!\n");
-  printf("Major is Clomputer Science!");
+  printf("Major is Clomputer Science!\n");
+  printf("Minor is Japanese.\n!");
 
   return 0;
 }
